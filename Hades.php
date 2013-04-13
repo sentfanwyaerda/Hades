c6a30d29d78a467fa09591b95d26a4c88c21d174
Sent fan Wyaerda <git@sent.wyaerda.org>
2013-04-14 00:48:27 +0200

    v8.0 initial alpha stage

---
+++ Hades.php
@@ -0,0 +1,4 @@
+<?php
+require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'Hades.settings.php');
+require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'module'.DIRECTORY_SEPARATOR.'Hades'.DIRECTORY_SEPARATOR.'Hades.php');
+?>
